21afdb878ef34288e4519496f47badc8ae0339f3
Maxime Lagresle <maxime@wellfound.com>
2024-04-19 19:07:44 +0200

    add tests

--- internal/provider/data_source_item_login_test.go
+++ internal/provider/data_source_item_login_test.go
@@ -53,16 +53,48 @@ func TestAccDataSourceItemLoginDeleted(t *testing.T) {
 				Check:  getObjectID("bitwarden_item_login.foo", &objectID),
 			},
 			{
-				Config: tfConfigProvider() + tfConfigDataItemLoginWithURLFilter("https://start_with/something"),
-				Check:  getObjectID("bitwarden_item_login.foo", &objectID),
-			},
-			{
 				Config: tfConfigProvider() + tfConfigResourceItemLoginSmall() + tfConfigDataItemLoginWithId(objectID),
 				PreConfig: func() {
 					err := bwTestClient(t).DeleteObject("item", objectID)
 					assert.NoError(t, err)
 				},
 				ExpectError: regexp.MustCompile("Error: object not found"),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceItemLoginBySearch(t *testing.T) {
+	var objectID string
+	resourceName := "bitwarden_item_login.foo"
+
+	ensureVaultwardenConfigured(t)
+
+	resource.UnitTest(t, resource.TestCase{
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: tfConfigProvider() + tfConfigResourceFolder() + tfConfigResourceItemLogin(),
+				Check: resource.ComposeTestCheckFunc(
+					checkItemLogin(resourceName),
+					getObjectID(resourceName, &objectID),
+				),
+			},
+			{
+				Config: tfConfigProvider() + tfConfigResourceFolder() + tfConfigResourceItemLogin() + tfConfigDataItemLoginWithSearchAndOrg("test-username"),
+				Check:  checkItemLogin("data.bitwarden_item_login.foo_data"),
+			},
+			{
+				Config: tfConfigProvider() + tfConfigResourceFolder() + tfConfigResourceItemLogin() + tfConfigResourceItemLoginDuplicate() + tfConfigDataItemLoginWithSearchAndOrg("test-username"),
+				Check:  checkItemLogin("data.bitwarden_item_login.foo_data"),
+			},
+			{
+				Config:      tfConfigProvider() + tfConfigResourceFolder() + tfConfigResourceItemLogin() + tfConfigResourceItemLoginDuplicate() + tfConfigDataItemLoginWithSearchOnly("test-username"),
+				ExpectError: regexp.MustCompile("Error: too many objects found"),
+			},
+			{
+				Config:      tfConfigProvider() + tfConfigResourceFolder() + tfConfigResourceItemLogin() + tfConfigDataItemLoginWithSearchAndOrg("missing-item"),
+				ExpectError: regexp.MustCompile("Error: no object found matching the filter"),
 			},
 		},
 	})
@@ -78,16 +110,37 @@ data "bitwarden_item_login" "foo_data" {
 `, id)
 }
 
-func tfConfigDataItemLoginWithURLFilter(url string) string {
+func tfConfigDataItemLoginWithSearchAndOrg(search string) string {
 	return fmt.Sprintf(`
 data "bitwarden_item_login" "foo_data" {
 	provider	= bitwarden
 
-	url = "%s"
+	search = "%s"
+	organization_id = "%s"
 }
-`, url)
+`, search, testOrganizationID)
 }
 
+func tfConfigDataItemLoginWithSearchOnly(search string) string {
+	return fmt.Sprintf(`
+data "bitwarden_item_login" "foo_data" {
+	provider	= bitwarden
+
+	search = "%s"
+}
+`, search)
+}
+
+func tfConfigResourceItemLoginDuplicate() string {
+	return `
+	resource "bitwarden_item_login" "foo_duplicate" {
+		provider 			= bitwarden
+
+		name 					= "another item with username 'test-username'"
+		username 			= "test-username"
+	}
+	`
+}
 func tfConfigDataItemLogin() string {
 	return `
 data "bitwarden_item_login" "foo_data" {
