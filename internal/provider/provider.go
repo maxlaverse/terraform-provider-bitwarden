17a8918347d475d79312c20e82005d0bfb479c1a
Maxime Lagresle <max@laverse.net>
2024-08-21 23:15:23 +0200

    re-enable sync in tests

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -256,9 +256,7 @@ func newBitwardenClient(d *schema.ResourceData, version string) (bw.Client, erro
 	}
 
 	if version == versionDev {
-		// During development, we disable Vault synchronization and retry backoffs to make some
-		// operations faster.
-		opts = append(opts, bw.DisableSync())
+		// During development, we disable retry backoffs to make some operations faster.
 		opts = append(opts, bw.DisableRetryBackoff())
 	}
 	bwExecutable, err := exec.LookPath("bw")
