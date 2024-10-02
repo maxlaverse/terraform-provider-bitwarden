5a8fa6eb6ee492b8a21db1c384dab854cdcc40d7
Maxime Lagresle <max@laverse.net>
2024-10-02 07:50:31 +0200

    prevent argon2 from crashing provider on error

--- internal/bitwarden/crypto/keybuilder/prelogin_key.go
+++ internal/bitwarden/crypto/keybuilder/prelogin_key.go
@@ -15,14 +15,31 @@ func BuildPreloginKey(masterPassword, email string, kdfConfig models.KdfConfigur
 }
 
 func buildKey(masterPassword, salt string, kdfConfig models.KdfConfiguration) (*symmetrickey.Key, error) {
+	var rawKey []byte
 	switch kdfConfig.KdfType {
 	case models.KdfTypePBKDF2_SHA256:
-		return symmetrickey.NewFromRawBytes(pbkdf2.Key([]byte(masterPassword), []byte(salt), kdfConfig.KdfIterations, 32, sha256.New))
+		rawKey = pbkdf2.Key([]byte(masterPassword), []byte(salt), kdfConfig.KdfIterations, 32, sha256.New)
 	case models.KdfTypeArgon2:
 		hashedSalt := sha256.New()
 		hashedSalt.Write([]byte(salt))
-		return symmetrickey.NewFromRawBytes(argon2.IDKey([]byte(masterPassword), hashedSalt.Sum(nil), uint32(kdfConfig.KdfIterations), uint32(kdfConfig.KdfMemory*1024), uint8(kdfConfig.KdfParallelism), 32))
+
+		var err error
+		err = func() (err error) {
+			defer func() {
+				if r := recover(); r != nil {
+					err = fmt.Errorf("argon2.IDKey() panicked: %v", r)
+				}
+			}()
+
+			rawKey = argon2.IDKey([]byte(masterPassword), hashedSalt.Sum(nil), uint32(kdfConfig.KdfIterations), uint32(kdfConfig.KdfMemory*1024), uint8(kdfConfig.KdfParallelism), 32)
+			return
+		}()
+		if err != nil {
+			return nil, err
+		}
 	default:
 		return nil, fmt.Errorf("unsupported KDF: '%d'", kdfConfig.KdfType)
 	}
+
+	return symmetrickey.NewFromRawBytes(rawKey)
 }
